d45bc7488c8a7f16f2ecfac44cc817d9776e1ebe
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-06-08 14:48:56 +0300

    Add cypress folder

---
+++ cypress/integration/e2e.test.js
@@ -0,0 +1,13 @@
+// e2e.test.js created with Cypress
+//
+// Start writing your Cypress tests below!
+// If you're unfamiliar with how Cypress works,
+// check out the link below and learn how to write your first test:
+// https://on.cypress.io/writing-first-test
+describe('Pokedex', function() {
+  it('front page can be opened', function() {
+    cy.visit('http://localhost:8080')
+    cy.contains('ivysaur')
+    cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
+  })
+})
