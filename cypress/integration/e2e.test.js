ba0ca8e13016492cf739423680bf521c61b9b489
Thananon Pongsuwan <thenightraven7407@gmail.com>
2021-06-08 14:51:18 +0300

    trying to fix cypress lint

--- cypress/integration/e2e.test.js
+++ cypress/integration/e2e.test.js
@@ -6,8 +6,11 @@
 // https://on.cypress.io/writing-first-test
 describe('Pokedex', function() {
   it('front page can be opened', function() {
+    // eslint-disable-next-line no-undef
     cy.visit('http://localhost:8080')
+    // eslint-disable-next-line no-undef
     cy.contains('ivysaur')
+    // eslint-disable-next-line no-undef
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
 })
